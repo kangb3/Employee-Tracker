a3b3485ce40dd7bc14d73ba8cf072e0aa7125a18
Baldeep Kang <bkang4612@gmail.com>
2020-11-20 17:21:05 -0800

    create queries file to store view queries

---
+++ db/queries.js
@@ -0,0 +1,38 @@
+const connection = require("./connection");
+
+class DB {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+
+  //Query to see all employees
+  viewAllEmployees() {
+    return this.connection.query(
+      `
+            SELECT 
+                EMP.ID,
+                EMP.first_name as 'First name',
+                EMP.last_name as 'Last name',
+                ER.title as 'Title',
+                ER.salary as 'Salary',
+                DEP.name as 'Department',
+                CONCAT(man.first_name, ' ', man.last_name) as 'Manager'
+
+            FROM 
+                employee AS MAN
+            RIGHT JOIN
+                employee AS EMP
+                ON MAN.id = EMP.manager_id
+            INNER JOIN 
+                empRole AS ER
+                ON EMP.ROLE_ID = ER.id
+            INNER JOIN  
+                department AS DEP  
+                ON ER.department_id = DEP.id
+            ORDER BY 
+                EMP.ROLE_ID
+        `
+    );
+  }
+}
