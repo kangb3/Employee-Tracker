dacf19685212ff03e8af5527eb3111a217a1b0e4
Baldeep Kang <bkang4612@gmail.com>
2020-11-20 17:15:36 -0800

    update schema with cascade delete

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,35 @@
-DROP DATABASE IF EXISTS employee_trackerDB;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+USE company_db;
 
-CREATE DATABASE employee_trackerDB;
 
-USE employee_trackerDB;
 
-CREATE TABLE department (
+CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
+
+
+CREATE TABLE empRole (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(8,2) NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
     department_id INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
+
+
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT NOT NULL,
-    manager_id INT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) references employee(id)
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES empRole(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
-
