927053aefc080f6622bec593c0af51a31dad3db5
Baldeep Kang <bkang4612@gmail.com>
2020-11-20 17:17:12 -0800

    updating data seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,28 +1,29 @@
--- Departments
-INSERT INTO department(name) values('Information Services');
-INSERT INTO department(name) values('Finance');
-INSERT INTO department(name) values('Parks & Recreation');
-
--- roles 
-INSERT INTO roles(title, salary, department_id) values('IT Manager', 100000.00, 1);
-INSERT INTO roles(title, salary, department_id) values('IT Analyst 1', 60000.00, 1);
-INSERT INTO roles(title, salary, department_id) values('IT Analyst 2', 70000.00, 1);
-
-INSERT INTO roles(title, salary, department_id) values('Finance Manager', 100000.00, 2);
-INSERT INTO roles(title, salary, department_id) values('Accountant', 85000.00, 2);
-
-INSERT INTO roles(title, salary, department_id) values('Parks Manager', 100000.00, 3);
-INSERT INTO roles(title, salary, department_id) values('Maintenance Worker', 55000.00, 3);
-INSERT INTO roles(title, salary, department_id) values('City Engineer', 75000.00, 3);
+USE company_db;
 
 
--- employees
-INSERT INTO employee(first_name, last_name, role_id) values('Joe', 'Johnson', 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) values('Michael', 'Krewson', 2, 1);
+/*Departments*/
+INSERT INTO department (name)
+VALUES("Leadership");
+INSERT INTO department (name)
+VALUES("Technology");
+INSERT INTO department (name)
+VALUES("Finance");
 
-INSERT INTO employee(first_name, last_name, role_id) values('Aubrey', 'Plaza', 4);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) values('Angela', 'Martin', 5, 2);
 
-INSERT INTO employee(first_name, last_name, role_id) values('Leslie', 'Knope', 6);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) values('Jerry', 'Gergich', 7, 4);
-INSERT INTO employee(first_name, last_name, role_id) values('Ron', 'Swanson', 8);
+/*Roles*/
+INSERT INTO empRole (title, salary, department_id)
+VALUES("Manager", 170000, 1);
+INSERT INTO empRole (title, salary, department_id)
+VALUES("Engineer", 130000, 2);
+INSERT INTO empRole (title, salary, department_id)
+VALUES("Analysis", 110000, 3);
+
+/*Employee data*/
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES("Ron", "Swanson", 1, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES("Leslie", "Knope", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES("Andy", "Dwyer", 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES("Ann", "Perkins", 2, 1);
